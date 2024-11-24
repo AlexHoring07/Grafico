40ef6a7205fa59b7c72da84a3e193cbda4d40360
Alex Horing <106245550+AlexHoring07@users.noreply.github.com>
2024-11-24 09:50:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const ctx = document.getElementById('streamingChart').getContext('2d');
+
 const streamingChart = new Chart(ctx, {
     type: 'pie',
     data: {
         labels: ['Netflix', 'Prime Video', 'Disney+', 'Max'],
         datasets: [{
+            label: 'Usuários de Streaming (milhões)',
             data: [230, 200, 150, 80],
             backgroundColor: ['#f44336', '#4caf50', '#2196f3', '#9c27b0'],
+            hoverOffset: 10
         }]
     },
     options: {
@@ -13,6 +16,9 @@ const streamingChart = new Chart(ctx, {
         plugins: {
             legend: {
                 position: 'top',
+                labels: {
+                    color: '#003c8f'
+                }
             },
             tooltip: {
                 enabled: true
